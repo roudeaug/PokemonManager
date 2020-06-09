53b3e869a046d9857255c512447aadc27df09411
groudeau <g.roudeau@gmail.com>
2020-06-09 15:21:42 +0200

    Add Generations and Pokemons components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenerationsComponent } from './components/generations/generations.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenerationsComponent,
+    PokemonsComponent
   ],
   imports: [
     BrowserModule,
